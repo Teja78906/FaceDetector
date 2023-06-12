91b940c2fb7341a681b597c7d0a91d0ee8db3584
Teja78906 <99752084+Teja78906@users.noreply.github.com>
2023-06-12 22:23:55 +0530

    Update faceDetector.py

--- faceDetector.py
+++ faceDetector.py
@@ -3,17 +3,19 @@ import numpy as np
 from PIL import Image
 import os
 
+# Preprocess an image by resizing, converting to grayscale, and applying histogram equalization
 def preprocess_image(image_path, target_size):
     img = Image.open(image_path).convert('L')
     img = img.resize(target_size, Image.LANCZOS)
-    
+
     # Apply histogram equalization
     img = np.array(img)
     img = cv2.equalizeHist(img)
-    
+
     preprocessed_img = np.array(img, 'uint8')
     return preprocessed_img
 
+# Train the face classifier using Fisherfaces algorithm
 def train_classifier(data_dir, target_size=(100, 100)):
     path = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
     faces = []
@@ -30,6 +32,7 @@ def train_classifier(data_dir, target_size=(100, 100)):
     clf.train(faces, ids)
     clf.write("classifier.yml")
 
+# Detect faces in an image and draw rectangles around them
 def detect(img, faceCascade, img_id, classifier, confidence_threshold=4000):
     color = {"blue": (255, 0, 0), "red": (0, 0, 255), "green": (0, 255, 0)}
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -50,21 +53,21 @@ def detect(img, faceCascade, img_id, classifier, confidence_threshold=4000):
             cv2.rectangle(img, (x, y), (x + w, y + h), color['red'], 2)
             cv2.putText(img, f"Not Matched ({confidence:.2f},{label})", (x, y - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
                         color['red'], 1, cv2.LINE_AA)
-#         else:
-#             cv2.rectangle(img, (x, y), (x + w, y + h), color['blue'], 2)
-#             cv2.putText(img, "Unknown", (x, y - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color['blue'], 1, cv2.LINE_AA)
 
     return img
 
+# Load the pre-trained face cascade classifier
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 img_id = 0
 video_capture = cv2.VideoCapture(0)
+
 # Set the desired output frame size
 output_width = 1200
 output_height = 800
 
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, output_width)
 video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, output_height)
+
 # Load or train the classifier
 if os.path.isfile("classifier.yml"):
     classifier = cv2.face.FisherFaceRecognizer_create()
@@ -86,3 +89,4 @@ while True:
 
 video_capture.release()
 cv2.destroyAllWindows()
+
